0f3cfd8011edf694223f6d8eadaaa194b1e39842
Anthony Smith <anthony@sticksnleaves.com>
2016-11-22 22:31:43 -0500

    Added excoveralls as a test dependency

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule ExShopify.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     preferred_cli_env: ["coveralls": :test, "coveralls.details": :test,
+                         "coveralls.html": :test, "coveralls.post": :test,
+                         "coveralls.travis": :test],
+     test_coverage: [tool: ExCoveralls]]
   end
 
   # Configuration for the OTP application
@@ -33,6 +37,7 @@ defmodule ExShopify.Mixfile do
      # dev
      {:ex_doc, ">= 0.0.0", only: :dev},
      #test
-     {:bypass, "~> 0.5.1", git: "https://github.com/PSPDFKit-labs/bypass", only: :test}]
+     {:bypass, "~> 0.5.1", git: "https://github.com/PSPDFKit-labs/bypass", only: :test},
+     {:excoveralls, "~> 0.5.7", only: :test}]
   end
 end
