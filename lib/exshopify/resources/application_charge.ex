957ff8cdf35aeec796bc319db97899036156b9dd
Anthony Smith <anthony@sticksnleaves.com>
2016-11-26 13:31:11 -0500

    Addned application charge endpoints

---
+++ lib/exshopify/resources/application_charge.ex
@@ -0,0 +1,63 @@
+defmodule ExShopify.ApplicationCharge do
+  @moduledoc """
+  A one-time charge to a shop.
+  """
+
+  import ExShopify.API
+  import ExShopify.Resource
+
+  @type application_charge_resource :: {:ok, %ExShopify.ApplicationCharge{}, %ExShopify.Meta{}}
+
+  defstruct [:confirmation_url, :created_at, :id, :name, :price, :return_url,
+             :status, :test, :updated_at]
+
+  @doc """
+  Activate a previously accepted one-time application charge.
+  """
+  def activate(session \\ nil, id, params) do
+    request(:post, "/application_charges/#{id}/activate.json", params, session)
+    |> serialize_resource(&serialize_single/1)
+  end
+
+  @doc """
+  Create a new one-time application charge.
+  """
+  @spec create(%ExShopify.Session{}, map) :: application_charge_resource | ExShopify.Resource.error
+  def create(session \\ nil, params) do
+    request(:post, "/application_charges.json", params, session)
+    |> serialize_resource(&serialize_single/1)
+  end
+
+  @doc """
+  Retrieve one-time application charge.
+  """
+  @spec find(%ExShopify.Session{}, integer | String.t, map) :: application_charge_resource | ExShopify.Resource.error
+  def find(session \\ nil, id, params \\ %{}) do
+    request(:get, "/application_charges/#{id}.json", params, session)
+    |> serialize_resource(&serialize_single/1)
+  end
+
+  @doc """
+  All charges that have been requested are retrieved by this request.
+  """
+  @spec list(%ExShopify.Session{}, map) :: [application_charge_resource] | ExShopify.Resource.error
+  def list(session \\ nil, params \\ %{}) do
+    request(:get, "/application_charges.json", params, session)
+    |> serialize_resource(&serialize_multi/1)
+  end
+
+  @doc false
+  def response_mapping do
+    %ExShopify.ApplicationCharge{}
+  end
+
+  # private
+
+  defp serialize_single(body) do
+    Poison.decode!(body, as: %{"application_charge" => response_mapping})["application_charge"]
+  end
+
+  defp serialize_multi(body) do
+    Poison.decode!(body, as: %{"application_charges" => [response_mapping]})["application_charges"]
+  end
+end
---
+++ test/exshopify/resources/application_charge_test.exs
@@ -0,0 +1,62 @@
+defmodule ExShopify.ApplicationCharge.Test do
+  use ExUnit.Case, async: true
+
+  setup do
+    bypass  = Bypass.open
+    session = ExShopify.Session.new(%{port: bypass.port})
+
+    {:ok, bypass: bypass, session: session}
+  end
+
+  describe "activate" do
+    test "endpoint", %{bypass: bypass, session: session} do
+      Bypass.expect(bypass, fn(conn) ->
+        assert conn.method == "POST"
+        assert conn.request_path == "/admin/application_charges/1/activate.json"
+
+        Plug.Conn.resp(conn, 200, "{\"application_charge\": {}}")
+      end)
+
+      ExShopify.ApplicationCharge.activate(session, 1, %{})
+    end
+  end
+
+  describe "create" do
+    test "endpoint", %{bypass: bypass, session: session} do
+      Bypass.expect(bypass, fn(conn) ->
+        assert conn.method == "POST"
+        assert conn.request_path == "/admin/application_charges.json"
+
+        Plug.Conn.resp(conn, 200, "{\"application_charge\": {}}")
+      end)
+
+      ExShopify.ApplicationCharge.create(session, %{})
+    end
+  end
+
+  describe "find" do
+    test "endpoint", %{bypass: bypass, session: session} do
+      Bypass.expect(bypass, fn(conn) ->
+        assert conn.method == "GET"
+        assert conn.request_path == "/admin/application_charges/1.json"
+
+        Plug.Conn.resp(conn, 200, "{\"application_charge\": {}}")
+      end)
+
+      ExShopify.ApplicationCharge.find(session, 1)
+    end
+  end
+
+  describe "list" do
+    test "endpoint", %{bypass: bypass, session: session} do
+      Bypass.expect(bypass, fn(conn) ->
+        assert conn.method == "GET"
+        assert conn.request_path == "/admin/application_charges.json"
+
+        Plug.Conn.resp(conn, 200, "{\"application_charges\": [{}]}")
+      end)
+
+      ExShopify.ApplicationCharge.list(session)
+    end
+  end
+end
