9ac35bba128b0dacef429137c4a7b76b5dcf0d3d
Anthony Smith <anthony@sticksnleaves.com>
2016-11-26 12:41:05 -0500

    Added moduledoc

--- lib/exshopify/resources/customer.ex
+++ lib/exshopify/resources/customer.ex
@@ -1,5 +1,9 @@
 defmodule ExShopify.Customer do
+  @moduledoc """
+  Information about the customer.
+  """
+
   defstruct [:accepts_marketing, :created_at, :email, :first_name, :id,
-             :last_name, :note, :order_count, :state, :total_spent, :updated_at,
+             :last_name, :note, :orders_count, :state, :total_spent, :updated_at,
              :tags]
 end
