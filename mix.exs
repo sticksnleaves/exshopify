14825932d5921e5e33230b9aed94f2bb263d0365
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-10-17 09:47:26 -0400

    FIRST!

---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule Exshopify.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :exshopify,
+      version: "0.4.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
