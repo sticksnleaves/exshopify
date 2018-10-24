7d5099c44d2e946ff2f55b8765b5456beb3c409d
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-10-24 09:30:38 -0400

    Add response struct

---
+++ lib/shopify/response.ex
@@ -0,0 +1,11 @@
+defmodule Shopify.Response do
+  @type t :: %__MODULE__{
+    body: binary,
+    headers: [{binary, binary}],
+    status_code: pos_integer
+  }
+
+  defstruct body: "",
+            headers: nil,
+            status_code: nil
+end
