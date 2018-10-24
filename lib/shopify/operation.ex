ce5953c6bb8ab5726fb16c746734c9a1729a238c
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-10-24 10:49:34 -0400

    Use request struct

--- lib/shopify/operation.ex
+++ lib/shopify/operation.ex
@@ -13,115 +13,57 @@ defmodule Shopify.Operation do
   def perform(operation, session, config) do
     {http_client, http_client_opts} = config.http_client
 
-    http_method = operation.http_method
-    url = build_url(operation, session, config)
-    headers = build_headers(session)
-    body = build_body(operation, config)
+    request = Shopify.Request.new(operation, session, config)
 
-    response = http_client.request(http_method, url, headers, body, http_client_opts)
-
-    parse(response, config)
+    case http_client.request(request, http_client_opts) do
+      {:ok, response} ->
+        handle_response(response, config)
+      otherwise ->
+        otherwise
+    end
   end
 
-  #
-  # private
-  #
-
-  defp build_url(operation, %Shopify.Session.Private{} = session, config) do
-    base_url = build_base_url(operation, session, config)
-
-    url = Map.put(base_url, :userinfo, "#{session.api_key}:#{session.password}")
-
-    URI.to_string(url)
-  end
-
-  defp build_url(operation, session, config) do
-    URI.to_string(build_base_url(operation, session, config))
-  end
-
-  defp build_base_url(operation, session, config) do
-    %URI{
-      host: "#{session.shop_name}.#{config.host}",
-      path: "/#{operation.path}",
-      port: config.port,
-      query: build_query(operation),
-      scheme: config.scheme
-    }
-  end
-
-  defp build_query(%{http_method: :get, params: params}) do
-    URI.encode_query(params)
-  end
-
-  defp build_query(_operation) do
-    nil
-  end
-
-  defp build_headers(%Shopify.Session.Public{access_token: access_token} = session)
-         when not is_nil(access_token)
+  defp handle_response(%{status_code: status_code} = response, config)
+       when status_code >= 200 and status_code < 400
   do
-    [{"X-Shopify-Access-Token", session.access_token}] ++ build_base_headers()
+    {:ok, parse_body(response.body, config), build_meta(response.headers)}
   end
 
-  defp build_headers(_session) do
-    build_base_headers()
+  defp handle_response(%{status_code: status_code} = response, config)
+       when status_code >= 400
+  do
+    {:error, parse_body(response.body, config)}
   end
 
-  defp build_base_headers do
-    [
-      {"Content-Type", "application/json"}
-    ]
-  end
-
-  defp build_body(%{http_method: :get}, _config) do
-    ""
-  end
-
-  defp build_body(operation, config) do
-    config.json_codec.encode!(operation.params)
+  defp parse_body(raw_body, config) do
+    case config.json_codec.decode(raw_body) do
+      {:ok, body} ->
+        body
+      {:error, _reason} ->
+        %{}
+    end
   end
 
   defp build_meta(headers) do
     Enum.reduce(headers, %Shopify.Meta{}, fn({k, v}, acc) ->
-      IO.inspect k
-      case k do
-        "X-Shopify-Shop-Api-Call-Limit" ->
-          Map.put(acc, :api_call_limit, v)
+      case String.downcase(k) do
+        "x-shopify-shop-api-call-limit" ->
+          parts = String.split(v, "/")
+
+          count =
+            parts
+            |> Enum.at(0)
+            |> String.to_integer()
+
+          limit =
+            parts
+            |> Enum.at(1)
+            |> String.to_integer()
+
+          Map.put(acc, :api_call_limit, {count, limit})
         _otherwise ->
           acc
       end
     end)
-  end
-
-  defp parse({:ok, %{status_code: status_code} = response}, config)
-         when status_code >= 200 and status_code < 400
-  do
-    body = response.body
-
-    meta = build_meta(response[:headers])
-
-    case config.json_codec.decode(body) do
-      {:error, _reason} ->
-        {:ok, %{}, meta}
-      {:ok, decoded_body} ->
-        {:ok, decoded_body, meta}
-    end
-  end
-
-  defp parse({:ok, %{status_code: status_code} = response}, config)
-         when status_code >= 400
-  do
-    body = response.body
-
-    case config.json_codec.decode(body) do
-      {:error, _reason} ->
-        {:error, %{}}
-      {:ok, decoded_body} ->
-        {:error, decoded_body}
-    end
-  end
-
-  defp parse(response, _config) do
-    response
   end
 end
