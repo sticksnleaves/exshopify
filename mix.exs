eb2124c226b95a1b4775eba8187029b84a3dd1b1
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-10-29 07:33:25 -0400

    Bump Elixir version requirement to 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Shopify.MixProject do
     [
       app: :exshopify,
       version: "0.4.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [plt_add_apps: [:gen_stage, :hackney]],
