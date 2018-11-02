18186692b147c90cf15ed127d2496c10c022fcd7
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-11-02 09:33:09 -0400

    Bump to v0.7.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shopify.MixProject do
   def project do
     [
       app: :exshopify,
-      version: "0.7.0",
+      version: "0.7.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
